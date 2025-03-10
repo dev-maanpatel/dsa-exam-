a77925ca409dbd92e7a91ae90dcf9d8080b8e4a1
dev-maanpatel <dev.maanpatel1@gmail.com>
2025-03-10 10:35:07 +0530

    q-2.cpp

--- q-2.cpp
+++ q-2.cpp
@@ -1,31 +1,35 @@
 #include<iostream>
-
 using namespace std;
 
-int main(){
+int main() {
+    int row, col, max;
 
-    int row , col , max = 0;
-
-    cout << "Enter the size of rows :";
+    cout << "Enter the size of rows: ";
     cin >> row;
 
-    cout << "Enter the size of col :";
+    cout << "Enter the size of cols: ";
     cin >> col;
 
-   int arr[row][col];
+    int arr[row][col];
 
-   for(int i = 0 ; i < row ; i++){
-    for (int j = 0 ; j < col ;  j++){
-        cout << "Enter the Number " <<"[" << i << "]" << "[" << j << "]" ;
-        cin >> arr[i][j];
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << "Enter the number of [" << i << "][" << j << "]: ";
+            cin >> arr[i][j];
+        }
     }
-    for(int i = 0 ; i < col ; i++){
-        for (int j = 0 ; j < row ; j++){
-            if(arr[i][j] < max){
-                cout << "The Largest element is : " << max ;
+
+    max = arr[0][0];
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (arr[i][j] > max) {
+                max = arr[i][j];
             }
         }
     }
-   }
+
+    cout << "The largest element is: " << max << endl;
+
     return 0;
 }
